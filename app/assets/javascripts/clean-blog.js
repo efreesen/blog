96376352ac8335d66c772b4201b951e835e5fd46
Caio Torres <efreesen@gmail.com>
2015-09-19 22:21:51 -0300

    Added progress of the reading bar

--- app/assets/javascripts/clean-blog.js
+++ app/assets/javascripts/clean-blog.js
@@ -1018,13 +1018,19 @@ jQuery(document).ready(function($) {
                     //if scrolling up...
                     if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
                         $('.navbar-custom').addClass('is-visible');
+                        $('progress').addClass('menu-visible');
                     } else {
                         $('.navbar-custom').removeClass('is-visible is-fixed');
+                        $('progress').removeClass('menu-visible');
                     }
                 } else {
                     //if scrolling down...
                     $('.navbar-custom').removeClass('is-visible');
-                    if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) $('.navbar-custom').addClass('is-fixed');
+                    $('progress').removeClass('menu-visible');
+
+                    if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) {
+                        $('.navbar-custom').addClass('is-fixed');
+                    }
                 }
                 this.previousTop = currentTop;
             });
