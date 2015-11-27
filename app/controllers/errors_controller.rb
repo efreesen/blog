e947dcb2bc7634c68fb28d50520e3a03af1a02ab
Caio Torres <caltorres@sp.ir7.com.br>
2015-11-27 14:45:36 -0200

    Adds cache to error pages

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,4 +1,7 @@
 class ErrorsController < Blog::BlogController
+  caches_action :not_found,             expires_in: 24.hour
+  caches_action :internal_server_error, expires_in: 24.hour
+
   def not_found
     render(:status => 404)
   end
