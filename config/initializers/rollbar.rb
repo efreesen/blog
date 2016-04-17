9779893154f32d8196ddd2e58e898f1c95d41d44
Caio Torres <efreesen@gmail.com>
2016-04-16 21:16:46 -0300

    Updated rollbar gem version

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -6,10 +6,9 @@ Rollbar.configure do |config|
   config.access_token = '66b461528018412a8400ffaf2fc84c40'
 
   # Here we'll disable in 'test':
-  if Rails.env.test? || Rails.env.development?
+  if Rails.env.test?
     config.enabled = false
   end
-  
 
   # By default, Rollbar will try to call the `current_user` controller method
   # to fetch the logged-in user object, and then call that object's `id`,
@@ -39,7 +38,7 @@ Rollbar.configure do |config|
   # config.use_async = true
   # Supply your own async handler:
   # config.async_handler = Proc.new { |payload|
-  #  Thread.new { Rollbar.process_payload_safely(payload) }
+  #  Thread.new { Rollbar.process_from_async_handler(payload) }
   # }
 
   # Enable asynchronous reporting (using sucker_punch)
